74bbcadbf3de9bf2204818bcc15585576918ce9d
olivierjan <olivierjan@yahoo.fr>
2018-09-25 23:10:30 +0200

    Working code, still no input but display welcome message !

--- Simple_memory_access/Simple_memory_access.ino
+++ Simple_memory_access/Simple_memory_access.ino
@@ -7,6 +7,8 @@ Start simulate some memory for 65c02 to Read/Write.
 #include "wiring.h"
 #include "msbasicrom.h"
 
+#define waitCycle 10000
+
 // Declare the Pins to use
 
 // Data Bus
@@ -56,36 +58,32 @@ void setup() {
 
   // Initialize some variables
 
-    addressL=0x0;
-    addressL_prev=0x0;
-    addressH=0x0;
-    addressH_prev=0x0;
-    currentClock=0;
-    rw=1;
-    clockCount=0;
+  addressL=0x0;
+  addressL_prev=0x0;
+  addressH=0x0;
+  addressH_prev=0x0;
+  currentClock=0;
+  rw=1;
+  clockCount=0;
 
-    // Bring the RESET LOW to get the 6502 in a reset state
-    digitalWrite(resetPin,LOW);
+  // Bring the RESET LOW to get the 6502 in a reset state
+  digitalWrite(resetPin,LOW);
 
-    // Initialize memory to 0xEA (NOP)
-    memset(mem,0x00,sizeof(mem));
-    memset(rom,0xEA,sizeof(rom));
+  // Initialize memory to 0xEA (NOP)
+  memset(mem,0x00,sizeof(mem));
 
+  delayMicroseconds(500);
+  digitalWrite(resetPin,HIGH);
 
 }
 
 
 void loop(){
 
-// If we have waited long enough for 6502 to init, stop reseting.
+  // Bring Clock LOW
+  GPIOA_PDOR &=0x0000;
 
-  if(clockCount == 8) {
-    digitalWrite(resetPin, HIGH);
-  }
-
-  // Bring Clock HIGH
-  GPIOA_PDOR |=1<<13;
-
+  delayMicroseconds (waitCycle);
 
   // Read the Address Bus LSB
   addressL = GPIOC_PDIR;
@@ -101,117 +99,77 @@ void loop(){
   addressH |= (GPIOB_PDIR >> 12) & 0xF0;
 
   // Does the 65C02 wants to Read or Write the data there ?
-  // rw=(GPIOA_PDIR>>11)&0x1;
-   rw=(GPIOA_PDIR>>12)&0x1;
+  rw=(GPIOA_PDIR>>12)&0x1;
 
-   // if (rw){
-   //   Serial.print("65c02 wants to READ\n");
-   // } else {
-   //   Serial.print("65c02 wants to WRITE\n");
-   //}
+  delayMicroseconds(waitCycle);
 
-  // if (addressL != addressL_prev || addressH != addressH_prev){
-  //   fullAddress=((uint16_t)addressH << 8 ) | addressL;
-  //   Serial.print("Address MSB, LSB: ");
-  //   Serial.print(addressH,HEX);
-  //   Serial.print(addressL,HEX);
-  //   Serial.print("-");
-  //   Serial.print(fullAddress,HEX);
-  //   Serial.print("\n");
-  //   Serial.print("Clock Counter: ");
-  //   Serial.print(clockCount);
-  //   Serial.print("\n");
-  // }
+  // Bring Clock HIGH
+  GPIOA_PDOR |=1<<13;
 
-  // if (addressH == 0xEA && addressH_prev == 0xFF && clockCount > 1) {
-  //   Serial.print("Rollover \n");
-  //   clockCount=0;
-  // }
-  // if (addressL == 0xEA && addressL_prev != 0xE9 && clockCount > 1) {
-  //   Serial.print("Reset detected !!!! \n");
-  //   clockCount=0;
-  // }
-  delay(500);
-
-  // Bring Clock LOW
-  GPIOA_PDOR &=0x0000;
-  //digitalWrite(clockPin,LOW);
-
+  delayMicroseconds(waitCycle);
   address = ((uint16_t)addressH << 8)| addressL;
-  Serial.print(address,HEX);
-  if ( address >= ROMADDRESS){
-    if (!rw) {
+
+  //Serial.print(address,HEX);
+
+  // Writing
+  if (rw){
+
+    //Serial.print(" R ");
+
+    //Configure pins for Output
+    GPIOD_PDDR=0xFF;
+
+      if ( address >= ROMADDRESS){
+
+        //Change address to map to ROM
+        address-=ROMADDRESS;
+
+        // Write data from ROM to Address Bus
+        GPIOD_PDOR= rom[address];
+        //Serial.print(rom[address],HEX);
+        //Serial.print("\n");
+      } else if (address >= SERIALADDRESS){
+          if (address == SERIALADDRESS) {
+            GPIOD_PDOR=0x2;
+            //Serial.print("02\n");
+          } else {
+            Serial.print("Reading from bad Serial address!!!\n");
+          }
+      } else {
+        GPIOD_PDOR= mem[address];
+        //Serial.print(mem[address],HEX);
+        //Serial.print("\n");
+      }
+  } else {
+
+    //Serial.print(" W ");
+    GPIOD_PDDR=0x00;
+
+    if ( address >= ROMADDRESS){
       // Trying to write to ROM - not good !!!
       Serial.print("!!! TRYING TO WRITE TO ROM !!!!\n");
+    } else if (address >= SERIALADDRESS){
+          // Read data from Address Bus and display directly if sent on data line(SERIALADDRESS + 1).
+          if (address== SERIALADDRESS + 1) {
+            //Serial.print(GPIOD_PDIR&0xFF,HEX);
+            Serial.write((char)GPIOD_PDIR&0xFF);
+            //Serial.print("\n");
+            //Serial.print(".");
+          } else {
+            //Serial.print(GPIOD_PDIR&0xFF,HEX);
+            //Serial.print("\n");
+          }
     } else {
-      //Configure pins for Output
-      GPIOD_PDDR=0xFF;
-
-      //Change address to map to ROM
-
-      address-=ROMADDRESS;
-      // Write data from ROM to Address Bus
-
-      GPIOD_PDOR= rom[address]&0xFF;
-      Serial.print(" Reading ROM address: ");
-      Serial.print(address,HEX);
-      Serial.print(" data: ");
-      Serial.print(rom[address],HEX);
-      Serial.print("actually sent: ");
-      Serial.print(GPIOD_PDIR, HEX);
-      Serial.print("\n");
-    }
-  } else if (address >= SERIALADDRESS){
-    if (!rw) {
-      // Configure pins for Input.
-      GPIOD_PDDR=0x00;
-
-      // Read data from Address Bus and display directly if sent on data line(SERIALADDRESS + 1).
-      if (address== SERIALADDRESS + 1) {Serial.print(GPIOD_PDIR & 0xFF);};
-    } else {
-      //Configure pins for Output
-      GPIOD_PDDR=0xFF;
-      Serial.print(" Reading Serial status\n");
-      // If checking status of Serial interface, return 0x2 for the time being.
-      if (address == SERIALADDRESS) { GPIOD_PDOR=0x2;}
-
+      mem[address]=GPIOD_PDIR;
+      //Serial.print(mem[address],HEX);
+      //Serial.print("\n");
     }
 
-  } else {
-    if (!rw) {
-      // Configure pins for Input.
-      GPIOD_PDDR=0x00;
-
-      // Read data from Address Bus into memory
-      // Address has been given in the last cycle and stored in addressH_prev
-      // and addressL_prev
-      Serial.print(" Writing RAM address: ");
-      Serial.print(address,HEX);
-      Serial.print(" data: ");
-      Serial.print(mem[address],HEX);
-      Serial.print("\n");
-      mem[address]=GPIOD_PDIR & 0xFF;
-    } else {
-      //Configure pins for Output
-      GPIOD_PDDR=0xFF;
-
-      // Write data from memory to Address Bus
-      // Address has been given in the last cycle and stored in addressH_prev
-      // and addressL_prev
-      Serial.print(" Reading RAM address: ");
-      Serial.print(address,HEX);
-      Serial.print(" data: ");
-      Serial.print(mem[address],HEX);
-      Serial.print("\n");
-      GPIOD_PDOR= mem[address]&0xFF;
-    }
   }
 
 
-
-
   // Wait a bit
-  delay(500);
+  delayMicroseconds(waitCycle);
 
   addressL_prev=addressL;
   addressH_prev=addressH;
