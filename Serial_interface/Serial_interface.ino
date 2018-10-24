117fa4da0defc83e0076fa4d08aebda97173f293
olivierjan <olivierjan@yahoo.fr>
2018-10-24 23:20:20 +0200

    Modified main MMU code to use external Serial.

--- Serial_interface/Serial_interface.ino
+++ Serial_interface/Serial_interface.ino
@@ -1,34 +1,103 @@
 // the setup function runs once when you press reset or power the board
 
-#define TESTEDPIN PORT_PA18 | PORT_PA19 | PORT_PA17
 #define READPORT PORTB
 #define DATAPORT PORTA
 
-#define NIBBLE_H PORT_PA19 | PORT_PA16 | PORT_PA18 | PORT_PA07
-#define NIBBLE_L PORT_PA23 | PORT_PA22 | PORT_PA10 | PORT_PA11
+#define NIBBLE_H PORT_PA19 | PORT_PA18 | PORT_PA17 | PORT_PA16
+#define NIBBLE_L PORT_PA11 | PORT_PA10 | PORT_PA09 | PORT_PA08
 
 #define RW PORT_PB02
 #define CLOCK PORT_PB08
 #define ADDRESS PORT_PB09
-#define ENABLE PORT_PB011
+#define ENABLE PORT_PB11
 
-volatile boolean enable=0;
-volatile boolean clock=0;
-volatile boolean rw=0;
-volatile uint_8 address=0;
+volatile uint32_t ACIAStatus=2;
+volatile uint32_t SendBuffer=0x00;
+volatile uint32_t ReceiveBuffer=0x00;
+volatile uint32_t DataBuffer=0x00;
+volatile uint32_t StatusBuffer=0x00;
+
 
 void setup() {
+
+  // Startup Serial (USB)
   Serial.begin(9600);
-  Serial.println(TESTEDPIN,HEX);
-  PORT->Group[TESTEDPORT].DIRSET.reg = TESTEDPIN;
-  PORT->Group[TESTEDPORT].OUTSET.reg = TESTEDPIN;
+
+  //Setup the ports
+
+  PORT->Group[READPORT].DIRSET.reg = 0x00;
+  PORT->Group[DATAPORT].DIRSET.reg = 0x00;
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-  Serial.println(TESTEDPIN,HEX);
-  Serial.println(PORT_PA13, HEX);
-  PORT->Group[TESTEDPORT].OUTTGL.reg = TESTEDPIN;
-  delay(100);              // wait for a second
+  // Read the CLOCK, ENABLE, RW and ADDRESS to see what is expected from us.
+  StatusBuffer=PORT->Group[READPORT].IN.reg;
+  DataBuffer=PORT->Group[READPORT].IN.reg;
+
+  // If we are requested to work
+  if (StatusBuffer & (CLOCK | ENABLE)) {
+
+      // Are we expected to send something ?
+      if (StatusBuffer & RW){
+        // Change PORT direction
+        PORT->Group[DATAPORT].DIRSET.reg= NIBBLE_H | NIBBLE_L;
+
+        // Send what we have to send and change status
+
+        if (StatusBuffer & ADDRESS){
+          PORT->Group[DATAPORT].OUTSET.reg = ACIAStatus;
+        } else {
+          PORT->Group[DATAPORT].OUTSET.reg = SendBuffer;
+        }
+
+        // Change back PORT direction.
+        PORT->Group[DATAPORT].DIRSET.reg=0x00;
+
+      } else {
+
+        // Store it for later processing
+        ReceiveBuffer=DataBuffer;
+      }
+
+  } else {
+
+    // If we are not requested to do anything
+
+      // Check if we have something to print or to write
+      ACIAStatus =(Serial.available()>0) ? 3 : 2;
+
+      // Serial Buffer contains something
+      if (ACIAStatus == 3) {
+        // Read Serial Port into the Buffer
+
+        SendBuffer=(uint32_t) Serial.read();
+      }
+
+      // Shift ACIAStatus to make it sendable through Teensy ports.
+      // We only need to shift the Low nibble as 2 or 3 won't require
+      // more than 4 bits anyway.
+
+      ACIAStatus=(ACIAStatus<<8);
+
+
+      // If we have received something, let's print it.
+
+      if (ReceiveBuffer){
+
+        // Clean the 32-bit buffer to make it 8-bit compatible.
+        // A bit of cleaning.
+        ReceiveBuffer &= ((NIBBLE_H)|(NIBBLE_L));
+
+        // Shift it to get everything in porsition
+        ReceiveBuffer = ((ReceiveBuffer >> 12)&0xF0)|((ReceiveBuffer >> 8)&0x0F);
+
+        // Now we can print the content of the buffer
+        Serial.print((char)ReceiveBuffer&0xFF);
+
+        // Cleanup the buffer
+        ReceiveBuffer=0x0;
+      }
+  }
 }
