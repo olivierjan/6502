4f81ea8dee81eb1e78c8a52891a9c17393497b95
olivierjan <olivierjan@yahoo.fr>
2018-11-03 22:49:41 +0100

    pic code

--- Serial_interface/Serial_interface.ino
+++ Serial_interface/Serial_interface.ino
@@ -24,32 +24,46 @@ void setup() {
   Serial.begin(9600);
 
   //Setup the ports
-
+  delay(2000);
+  Serial.println("Setting up pins");
   PORT->Group[READPORT].DIRSET.reg = 0x00;
   PORT->Group[DATAPORT].DIRSET.reg = 0x00;
+  pinMode(9, INPUT);
+  attachInterrupt(9, answerEnablesignal, HIGH);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-
-  // Read the CLOCK, ENABLE, RW and ADDRESS to see what is expected from us.
+void answerEnablesignal(){
+  Serial.println("Enabled!");
+  // Read data from the bus
+  StatusBuffer=0x00;
+  DataBuffer=0x00;
   StatusBuffer=PORT->Group[READPORT].IN.reg;
-  DataBuffer=PORT->Group[READPORT].IN.reg;
+  DataBuffer=PORT->Group[DATAPORT].IN.reg;
+  Serial.print("StatusBuffer: ");
+  Serial.print((uint32_t)StatusBuffer,HEX);
+  Serial.print("\n");
 
-  // If we are requested to work
-  if (StatusBuffer & (CLOCK | ENABLE)) {
+  Serial.print("DataBuffer: ");
+  Serial.print((uint32_t)DataBuffer,HEX);
+  Serial.print("\n");
 
+
+  if (StatusBuffer & CLOCK) {
+    Serial.println("Clock is High");
       // Are we expected to send something ?
       if (StatusBuffer & RW){
+
         // Change PORT direction
         PORT->Group[DATAPORT].DIRSET.reg= NIBBLE_H | NIBBLE_L;
 
         // Send what we have to send and change status
 
         if (StatusBuffer & ADDRESS){
+          Serial.println(ACIAStatus,HEX);
           PORT->Group[DATAPORT].OUTSET.reg = ACIAStatus;
         } else {
           PORT->Group[DATAPORT].OUTSET.reg = SendBuffer;
+          SendBuffer=0x00;
         }
 
         // Change back PORT direction.
@@ -58,18 +72,52 @@ void loop() {
       } else {
 
         // Store it for later processing
+        Serial.println("Received:"+(uint32_t)DataBuffer);
         ReceiveBuffer=DataBuffer;
       }
-
-  } else {
-
+    }
+}
+// the loop function runs over and over again forever
+void loop() {
+  //
+  // // Read the CLOCK, ENABLE, RW and ADDRESS to see what is expected from us.
+  // StatusBuffer=PORT->Group[READPORT].IN.reg;
+  // DataBuffer=PORT->Group[READPORT].IN.reg;
+  //
+  // // If we are requested to work
+  // if (StatusBuffer & (CLOCK | ENABLE)) {
+  //
+  //     // Are we expected to send something ?
+  //     if (StatusBuffer & RW){
+  //       // Change PORT direction
+  //       PORT->Group[DATAPORT].DIRSET.reg= NIBBLE_H | NIBBLE_L;
+  //
+  //       // Send what we have to send and change status
+  //
+  //       if (StatusBuffer & ADDRESS){
+  //         PORT->Group[DATAPORT].OUTSET.reg = ACIAStatus;
+  //       } else {
+  //         PORT->Group[DATAPORT].OUTSET.reg = SendBuffer;
+  //       }
+  //
+  //       // Change back PORT direction.
+  //       PORT->Group[DATAPORT].DIRSET.reg=0x00;
+  //
+  //     } else {
+  //
+  //       // Store it for later processing
+  //       ReceiveBuffer=DataBuffer;
+  //     }
+  //
+  // } else {
+  //
     // If we are not requested to do anything
 
       // Check if we have something to print or to write
       ACIAStatus =(Serial.available()>0) ? 3 : 2;
 
       // Serial Buffer contains something
-      if (ACIAStatus == 3) {
+      if ((ACIAStatus == 3) && (SendBuffer == 0)) {
         // Read Serial Port into the Buffer
 
         SendBuffer=(uint32_t) Serial.read();
@@ -99,5 +147,6 @@ void loop() {
         // Cleanup the buffer
         ReceiveBuffer=0x0;
       }
-  }
+//  }
+
 }
