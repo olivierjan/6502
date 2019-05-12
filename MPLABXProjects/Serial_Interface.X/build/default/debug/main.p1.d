a234908343853140ec53e9e36a31e9e163f2dd8e
Olivier Jan <olivierjan@yahoo.fr>
2019-05-12 18:35:30 +0200

    Added proper Serial tests
    Added proper Serial tests for PIC

---
+++ MPLABXProjects/Serial_Interface.X/build/default/debug/main.p1.d
@@ -0,0 +1,7 @@
+build/default/debug/main.p1:  \
+main.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/uart1.h 
---
+++ MPLABXProjects/Serial_Interface.X/build/default/debug/mcc_generated_files/uart1.p1.d
@@ -0,0 +1,3 @@
+build/default/debug/mcc_generated_files/uart1.p1:  \
+mcc_generated_files/uart1.c  \
+mcc_generated_files/uart1.h 
