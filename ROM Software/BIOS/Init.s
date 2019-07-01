9d8306a9c03aebc969911097335246df53313f1d
Olivier Jan <olivierjan@yahoo.fr>
2019-07-01 17:23:30 +0200

    Update Init.s

--- ROM Software/BIOS/Init.s
+++ ROM Software/BIOS/Init.s
@@ -19,35 +19,35 @@
 BIOSCHOUT				EXT
 BIOSCHGET				EXT
 BIOSCFGACIA				EXT
-BIOSCHISCTRLC			EXT
+BIOSCHISCTRLC				EXT
 
 
 MONITOR					EQU			$C000				; Let s see later the Monitor
-BASIC 				EQU			$C000           	; And Basic Entry points
-STACKTOP 				EQU 		#$F8				; Like a good old Apple //
+BASIC 					EQU			$C000           		; And Basic Entry points
+STACKTOP 				EQU 			#$F8				; Like a good old Apple //
 
 *-------------------------------------------------------------------------------
 *-- Entry point : Reset Vector
 *-------------------------------------------------------------------------------
 
-RESET					ENT								; Declare Global
+RESET						ENT						; Declare Global
 						LDX     	STACKTOP			; Load X with new top of stack value
-						TXS								; Set the stack pointer
+						TXS						; Set the stack pointer
 
-						JSR			BIOSCFGACIA			; Configure ACIA
+						JSR		BIOSCFGACIA			; Configure ACIA
 
-						LDY			#0					; Initialize counter
-]LOOP					LDA 		STARTUPMESSAGE0,Y	; Get character at counter
-						BEQ			MENU				; If we're done go get user choice
-						JSR			BIOSCHOUT			; else display the character
-						INY								; Move to next character
+						LDY		#0				; Initialize counter
+]LOOP						LDA 		STARTUPMESSAGE0,Y		; Get character at counter
+						BEQ		MENU				; If we're done go get user choice
+						JSR		BIOSCHOUT			; else display the character
+						INY						; Move to next character
 						BNE 		]LOOP
 
-MENU					LDY			#0					; Initialize counter
-]LOOP					LDA 		STARTUPMESSAGE1,Y	; Get character at counter
-						BEQ			USERINPUT			; If we're done go get user choice
-						JSR			BIOSCHOUT			; else display the character
-						INY								; Move to next character
+MENU						LDY		#0				; Initialize counter
+]LOOP						LDA 		STARTUPMESSAGE1,Y		; Get character at counter
+						BEQ		USERINPUT			; If we're done go get user choice
+						JSR		BIOSCHOUT			; else display the character
+						INY						; Move to next character
 						BNE 		]LOOP
 
 						
@@ -59,11 +59,11 @@ USERINPUT				JSR			BIOSCHGET			; Read user input
 						BCC 		USERINPUT			; Retry until we get something
 						AND 		#$DF				; Convert to UPPER case
 						CMP 		#'B'				; BASIC ?
-						BEQ			GOBASIC 			; Let's go for WARM STAFT
+						BEQ		GOBASIC 			; Let's go for BASIC
 						CMP 		#'M'				; MONITOR ?
 						BNE 		RESET 				; Something else ? Restart all...
-						JMP 		MONITOR         	; Let's go !
-GOBASIC 				JMP 		BASIC 			; Let's go...Warmer
+						JMP 		MONITOR         		; Let's go !
+GOBASIC 					JMP 		BASIC 				; Jump to BASIC entry point
 
 STARTUPMESSAGE0			ASC 		'-----------------------',0D,0A,'---  OJ',27,'s SBC V0.1  ---',0D,0A,'-----------------------',00
 STARTUPMESSAGE1			ASC 		0D,0A,0D,0A,'    1. [B]ASIC',0D,0A,'    2. [M]ONITOR',0D,0A,00
